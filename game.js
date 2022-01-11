830777d2687d7fdf4d097e55ab55dd9c4e3a77a9
VanbSanne <sannevanbrabant@hotmail.com>
2022-01-11 12:11:43 +0100

    added compareLetters function

--- game.js
+++ game.js
@@ -1,19 +1,19 @@
 var key = "";
 var word = [];
 var blocks = ["letter-00","letter-01","letter-02","letter-03","letter-04"]
+var refWord = ["d","a","n","c","e"];
 
-clearBlocks();
+
     document.addEventListener('keydown', function(event)
     {
-
         var key = event.key.toLowerCase();
-        if((isLetter(key)==true) && (word.length<5)){
+        if((isLetter(key)==true&&key.length==1) && (word.length<5)){
             word.push(key);
             document.getElementById(blocks[word.length-1]).innerHTML = key;
             console.log(word);
-        } else{
+        } else if(word.length>=4){
             compareLetters(word);
-        }   
+        }  
     })
 
 function isLetter(key){ //is it a letter between a and z?
@@ -27,12 +27,16 @@ function isLetter(key){ //is it a letter between a and z?
 }
 
 function compareLetters(){
-
-}
-
-function clearBlocks()
-{
-    for (i=0;i>blocks.length;i++){
-        blocks[i].innerHTML = "";
+    for(i=0;i<word.length;i++){
+        for(j=0;j<refWord.length;j++){
+            if(word[i]==refWord[i]){
+                console.log("same letter and place",word[i],refWord[i]);
+            } else if(word[i]==refWord[j]){
+                console.log("same letter",word[i],refWord[j]);
+            } else{
+                console.log("not a common letter.",word[i],refWord[j]);
+            }
+        }
     }
+    //console.log(word.charAt(2));
 }

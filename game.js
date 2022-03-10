d0b3b04c4fee969550e4668e04084e75794fc121
vanbsanne <sannevanbrabant@hotmail.com>
2022-03-10 21:51:41 +0100

    changed username

--- game.js
+++ game.js
@@ -1,41 +1,65 @@
 var key = "";
 var word = [];
-var blocks0 = ["letter-00","letter-01","letter-02","letter-03","letter-04"]
-var blocks1 = ["letter-10","letter-11","letter-12","letter-13","letter-14"]
-var blocks2 = ["letter-20","letter-21","letter-22","letter-23","letter-24"]
-var blocks3 = ["letter-30","letter-31","letter-32","letter-33","letter-34"]
-var blocks4 = ["letter-40","letter-41","letter-42","letter-43","letter-44"]
-var blocks5 = ["letter-50","letter-51","letter-52","letter-53","letter-54"]
-var refWord = ["d","a","n","c","e"];
- 
-main(); 
+var refWord = words[Math.trunc(Math.random()*words.length)]; /*trunc= delete decimal part */
+var currentWord = 0;
+var currentLetter = 0;
+var gameSet = false;
+var winner = false;
 
-function main(){
-    listening(blocks0);
-    // var blocks=[blocks0,blocks1,blocks2,blocks3,blocks4,blocks5];
-    // for(i=0;i<5;i++){
-    //     listening(blocks[i]);
-    // }
-}
 
-function listening(blocks){
-    document.addEventListener('keydown', function(event)
-    {
-        var key = event.key.toLowerCase();
-        if((isLetter(key)==true&&key.length==1) && (word.length<5)){
-            word.push(key);
-            document.getElementById(blocks[word.length-1]).innerHTML = key;
-            console.log(word);
-        } else if(word.length>=4){
-            if(word.toString() === refWord.toString()){
+document.addEventListener('keydown', function(event)
+{
+    if(gameSet){
+        return;
+    }
+    var key = event.key.toLowerCase();
+    if(key=="backspace" && currentLetter>0){
+        word.pop();
+        currentLetter--;
+        newBlocks = "letter-"+currentWord+currentLetter;
+        document.getElementById(newBlocks).innerHTML = "";
+    }
+    console.log(key);
+    var newBlocks = "letter-"+currentWord+currentLetter;
+    if((isLetter(key)&&key.length==1)){
+        word.push(key);
+        document.getElementById(newBlocks).innerHTML = key;
+        console.log(currentLetter);
+        currentLetter++;
+        if(currentLetter>4){
+            compareLetters();
+            if(word.join("") === refWord.toString()){
                 console.log("This is the correct word");
+                gameSet=true;
+                winner=true;
+                alert("AYYY");
             } else{
-                console.log("This is not the correct word");
-                compareLetters(blocks);
+                if(words.includes(word.join("")))
+                {
+                    console.log("This is not the correct word");
+                    
+                    currentWord++;
+                    if(currentWord==6){
+                        gameSet=true;
+                        winner=false;
+                        alert("NOOO");
+                    }
+                    currentLetter = 0;
+                    word.length = 0;
+                }
+                else{
+                    alert("NOT A WORD!"); 
+                    currentLetter=0;
+                    word.length = 0;
+                    for(i=0;i<5;i++){
+                        newBlocks = "letter-"+currentWord+i;
+                        document.getElementById(newBlocks).innerHTML = "";
+                    } 
+                }
             }
-        }  
-    })
-}
+        }
+    }   
+})
 
 function isLetter(key){ //is it a letter between a and z?
     if (key >= 'a' && key <= 'z'){
@@ -47,18 +71,15 @@ function isLetter(key){ //is it a letter between a and z?
     }
 }
 
-function compareLetters(blocks){      //is position and letter ok?
+function compareLetters(){      //is position and letter ok?
     for(i=0;i<word.length;i++){
+        var newBlocks = "letter-"+currentWord+i;
         for(j=0;j<refWord.length;j++){
             if(word[i]==refWord[i]){
-                console.log("same letter and place",word[i],refWord[i]);
-                document.getElementById(blocks[i]).classList.add("correct");
+                document.getElementById(newBlocks).classList.add("correct");
             } else if(word[i]==refWord[j]){
-                console.log("same letter",word[i],refWord[j]);
-                document.getElementById(blocks[i]).classList.add("correct-pos");
-            } else{
-                console.log("not a common letter.",word[i],refWord[j]);
+                document.getElementById(newBlocks).classList.add("correct-pos");
             }
         }
-    } word.length = 0;
+    }
 }
